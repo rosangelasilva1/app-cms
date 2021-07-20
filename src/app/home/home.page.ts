7c58d8e6ae4755bdfe8e5011f4b052c9b0039347
rosangela <roliveirasv@gmail.com>
2021-07-20 12:51:14 -0300

    Adicionado plugin camera

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 
 @Component({
   selector: 'app-home',
@@ -8,5 +9,27 @@ import { Component } from '@angular/core';
 export class HomePage {
 
   constructor() {}
+  imagem:string=""
+  private camera: Camera = new Camera();
+
+  abrirCamera(){
+
+    const options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.FILE_URI,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE
+    }
+
+    this.camera.getPicture(options).then((imageData) => {
+     // imageData is either a base64 encoded string or a file URI
+     // If it's base64 (DATA_URL):
+     let base64Image = 'data:image/jpeg;base64,' + imageData;
+     this.imagem = base64Image
+    }, (err) => {
+     // Handle error
+    });
+
+  }
 
 }
