e0adac9d34517c3ca36901589c1cf150a6e83c80
rosangela <roliveirasv@gmail.com>
2021-07-20 13:14:47 -0300

    Alteração para mostrar imagem na tela

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -16,7 +16,7 @@ export class HomePage {
 
     const options: CameraOptions = {
       quality: 100,
-      destinationType: this.camera.DestinationType.FILE_URI,
+      destinationType: this.camera.DestinationType.DATA_URL,
       encodingType: this.camera.EncodingType.JPEG,
       mediaType: this.camera.MediaType.PICTURE
     }
